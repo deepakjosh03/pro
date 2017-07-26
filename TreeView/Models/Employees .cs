c8b3fc28f42fc0ae1d4bfd324fe402bb51fc36eb
deepakjosh03 <30409124+deepakjosh03@users.noreply.github.com>
2017-07-26 17:49:29 +0530

    Add project files.

---
+++ TreeView/Models/Employees .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TreeView.Models
+{
+    public class Employees
+    {
+        [Required]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public int DepartmentID { get; set; } //Foreign Key  
+        public Department Department { get; set; } //Navigation Property  
+    }
+}
